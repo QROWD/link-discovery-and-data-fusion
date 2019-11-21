3da35dc700b5ca9b606679d7079eea750f70b884
BonaBeavis <gordian.dziwis@gmail.com>
2019-11-21 20:20:56 +0100

    Fix URIs for qrowd dataset

--- datasets/qrowd/toJson.py
+++ datasets/qrowd/toJson.py
@@ -1,14 +1,9 @@
 import json
-import pandas
-import pickle
 import sys
-from shapely.geometry import Point
+import pickle
 
-file = open(sys.argv[1], 'rb')
-data = pickle.load(file)
-file.close()
-jsonData = json.loads(data.to_json(orient="table"))["data"]
-for point in jsonData:
-    point["wkt"] = Point(point["lng"], point["lat"]).wkt
-print(json.dumps(jsonData))
-
+FILE = open(sys.argv[1], 'rb')
+DATA = pickle.load(FILE)
+FILE.close()
+JSON_DATA = json.loads(DATA.to_json(orient="table"))["data"]
+print(json.dumps(JSON_DATA))
